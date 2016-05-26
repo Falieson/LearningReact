14a55100b0e0df0519a36d5b8c93a4313efab935
Florian Mettetal <florian.mettetal@gmail.com>
2016-05-26 14:31:57 -0700

    Redux: Writing a Todo List Reducer (Toggling a Todo)   - Made minor mod. for Object.assign instead of object spread (es7+)

--- Abramov2016/todo.jsx
+++ Abramov2016/todo.jsx
@@ -10,6 +10,15 @@ const todos = (state = [], action) => {
           completed: false,
         }
       ];
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if(todo.id !== action.id) {
+          return todo;
+        }
+        return Object.assign({}, todo, {
+          completed: !todo.completed,
+        });
+      });
     default:
       return state;
   }
@@ -38,5 +47,44 @@ const testAddTodo = () => {
   ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: false,
+    }
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go Shopping',
+      completed: true,
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+};
+
 testAddTodo();
+testToggleTodo();
 console.log(`All tests passed.`);
