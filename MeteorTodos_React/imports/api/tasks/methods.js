110e14d677df6bce07cf6439255e41d9bebb568e
Florian Mettetal <florian.mettetal@gmail.com>
2016-05-31 10:12:46 -0700

    Added a new test for inserting a task

--- MeteorTodos_React/imports/api/tasks/methods.js
+++ MeteorTodos_React/imports/api/tasks/methods.js
@@ -17,7 +17,7 @@ Meteor.methods({
       text,
       createdAt: new Date(),
       owner: this.userId,
-      username: Meteor.users.findOne(this.userId).username,
+      username: this.username? this.username: Meteor.users.findOne(this.userId).username,
     });
   },
   'tasks.remove'(taskId) {
@@ -31,29 +31,29 @@ Meteor.methods({
 
     Tasks.remove(taskId);
   },
-  // 'tasks.setChecked'(taskId, setChecked) {
-  //   check(taskId, String);
-  //   check(setChecked, Boolean);
-  //
-  //   const task = Tasks.findOne(taskId);
-  //   if (task.private && task.owner !== this.userId) {
-  //     // If the task is private, make sure only the owner can check it off
-  //     throw new Meteor.Error('not-authorized');
-  //   }
-  //
-  //   Tasks.update(taskId, { $set: { checked: setChecked } });
-  // },
-  // 'tasks.setPrivate'(taskId, setToPrivate) {
-  //   check(taskId, String);
-  //   check(setToPrivate, Boolean);
-  //
-  //   const task = Tasks.findOne(taskId);
-  //
-  //   // Make sure only the task owner can make a task private
-  //   if (task.owner !== this.userId) {
-  //     throw new Meteor.Error('not-authorized');
-  //   }
-  //
-  //   Tasks.update(taskId, { $set: { private: setToPrivate } });
-  // },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== this.userId) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+  },
 });
--- MeteorTodos_React/imports/api/tasks/methods.tests.js
+++ MeteorTodos_React/imports/api/tasks/methods.tests.js
@@ -11,19 +11,36 @@ if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
       const userId = Random.id();
-      let taskId;
+      const username = 'tmeasday';
+      const task = {
+        text: 'test task',
+        createdAt: new Date(),
+        owner: userId,
+        username,
+      };
 
       beforeEach(() => {
-        Tasks.remove({});
-        taskId = Tasks.insert({
-          text: 'test task',
-          createdAt: new Date(),
-          owner: userId,
-          username: 'tmeasday',
-        });
+        Tasks.remove({}); // Deletes all items in the collection
+      });
+
+      it('can insert new task', () => {
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const insertTask = Meteor.server.method_handlers['tasks.insert'];
+
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { userId, username };
+
+        // Run the method with `this` set to the fake invocation
+        insertTask.apply(invocation, [task.text]);
+
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 1);
       });
 
       it('can delete owned task', () => {
+        const taskId = Tasks.insert(task);
+
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const deleteTask = Meteor.server.method_handlers['tasks.remove'];
@@ -37,6 +54,9 @@ if (Meteor.isServer) {
         // Verify that the method does what we expected
         assert.equal(Tasks.find().count(), 0);
       });
-    });
+    }); // END describe methods
   });
+
+
+
 }
